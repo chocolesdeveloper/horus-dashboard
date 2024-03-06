7f896713efe67beff84e874dc788784e0996d804
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-03-06 17:33:33 -0300

    feat: fix console

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -13,19 +13,16 @@ export const authOptions: NextAuthOptions = {
       },
 
       async authorize(credentials) {
-        const response = await fetch(
-          `https://dashborad-financeiro-engenharia.vercel.app/api/login`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
+        const response = await fetch(`${process.env.NEXT_AUTH_URL}/api/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-        );
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+        });
 
         const user = await response.json();
 
