0ec99e6871b1abc2664a39d8cc77598ac37f83a1
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-03-11 18:27:05 -0300

    feat: update partial fiels fixed

--- app/(admin)/contract/_action/update-partial-field.ts
+++ app/(admin)/contract/_action/update-partial-field.ts
@@ -8,7 +8,7 @@ import { revalidatePath } from "next/cache";
 interface UpdatePartialFieldProps {
   contractId: string;
   executedDate: Date | undefined;
-  executedValue: number;
+  executedValue: string;
 }
 
 export async function UpdatePartialField({
@@ -27,12 +27,13 @@ export async function UpdatePartialField({
   }
 
   const valueIsFinished =
-    contract?.contractValue - ((contract?.executedValue ?? 0) + executedValue) <
+    Number(contract?.contractValue) -
+      (Number(contract?.executedValue) + Number(executedValue)) <
     0;
 
   const executedValueFinish = valueIsFinished
     ? contract.contractValue
-    : (contract?.executedValue ?? 0) + executedValue;
+    : Number(contract?.executedValue) + Number(executedValue);
 
   const contractUpdate = await db.contract.update({
     where: {
