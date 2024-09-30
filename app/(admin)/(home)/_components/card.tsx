00825e8bc311457f5ea7f046a8cb4309fa8511a6
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-09-30 16:31:59 -0300

    feat: ensure that the component has been re-rendered for countup

--- app/(admin)/(home)/_components/card.tsx
+++ app/(admin)/(home)/_components/card.tsx
@@ -15,6 +15,7 @@ import {
 } from "@/components/ui/card";
 
 import CountUp from "react-countup";
+import { useEffect, useState } from "react";
 
 interface CardProps {
   description: string;
@@ -23,22 +24,30 @@ interface CardProps {
 }
 
 export function Card({ description, value, type }: CardProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <CardUI className="flex h-32 flex-col justify-between p-5 shadow-lg">
       <CardDescription>{description}</CardDescription>
       <CardHeader className="p-0">
         <CardTitle className="flex items-center justify-between">
-          <CountUp
-            start={0}
-            end={Number(value)}
-            decimals={type !== "contract" ? 2 : 0}
-            separator="."
-            decimal=","
-            prefix={type !== "contract" ? "R$ " : ""}
-            duration={4}
-          >
-            {({ countUpRef, start }) => <span ref={countUpRef} />}
-          </CountUp>
+          {mounted && (
+            <CountUp
+              start={0}
+              end={Number(value)}
+              decimals={type !== "contract" ? 2 : 0}
+              separator="."
+              decimal=","
+              prefix={type !== "contract" ? "R$ " : ""}
+              duration={4}
+            >
+              {({ countUpRef, start }) => <span ref={countUpRef} />}
+            </CountUp>
+          )}
 
           {type === "expense" && <TrendingUpIcon className="text-green-500" />}
           {type === "income" && <TrendingDownIcon className="text-red-500" />}
