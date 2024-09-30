f7a837a0c7422ecc3412544e20ecf15745ad9022
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-09-30 16:41:09 -0300

    feat: create gridCard component

--- app/(admin)/(home)/_components/card.tsx
+++ app/(admin)/(home)/_components/card.tsx
@@ -32,31 +32,35 @@ export function Card({ description, value, type }: CardProps) {
   }, []);
 
   return (
-    <CardUI className="flex h-32 flex-col justify-between p-5 shadow-lg">
-      <CardDescription>{description}</CardDescription>
-      <CardHeader className="p-0">
-        <CardTitle className="flex items-center justify-between">
-          {mounted && (
-            <CountUp
-              start={0}
-              end={value}
-              decimals={type !== "contract" ? 2 : 0}
-              separator="."
-              decimal=","
-              prefix={type !== "contract" ? "R$ " : ""}
-              duration={4}
-              delay={2}
-            >
-              {({ countUpRef }) => <span ref={countUpRef} />}
-            </CountUp>
-          )}
+    <div>
+      <CardUI className="flex h-32 flex-col justify-between p-5 shadow-lg">
+        <CardDescription>{description}</CardDescription>
+        <CardHeader className="p-0">
+          <CardTitle className="flex items-center justify-between">
+            {value}
 
-          {type === "expense" && <TrendingUpIcon className="text-green-500" />}
-          {type === "income" && <TrendingDownIcon className="text-red-500" />}
-          {type === "contract" && <ReceiptText className="" />}
-          {type === "total" && <Receipt className="" />}
-        </CardTitle>
-      </CardHeader>
-    </CardUI>
+            {type === "expense" && (
+              <TrendingUpIcon className="text-green-500" />
+            )}
+            {type === "income" && <TrendingDownIcon className="text-red-500" />}
+            {type === "contract" && <ReceiptText className="" />}
+            {type === "total" && <Receipt className="" />}
+          </CardTitle>
+        </CardHeader>
+      </CardUI>
+      {mounted && (
+        <CountUp
+          start={0}
+          end={value}
+          decimals={type !== "contract" ? 2 : 0}
+          separator="."
+          decimal=","
+          prefix={type !== "contract" ? "R$ " : ""}
+          duration={4}
+        >
+          {({ countUpRef }) => <span ref={countUpRef} />}
+        </CountUp>
+      )}
+    </div>
   );
 }
