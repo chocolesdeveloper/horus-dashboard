06c0abfca4690d38ea5e69a31e90f359a2fbf683
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-10-10 20:25:38 -0300

    'fix'

--- components/input-simple.tsx
+++ components/input-simple.tsx
@@ -34,7 +34,7 @@ export function InputSimple({
         />
         {errors[name] && (
           <span className="text-center text-xs text-red-500">
-            {errors ? String(errors?.[name].message) : ""}
+            {errors[name] ? String(errors?.[name]?.message) : ""}
           </span>
         )}
       </div>
